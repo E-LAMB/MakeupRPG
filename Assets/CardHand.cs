6f87ece4afb4cf11b89ee69f3689863a6866b617
E-LAMB <aatkinson2022@elam.co.uk>
2023-12-06 11:14:39 +0000

    Began

--- Assets/CardHand.cs
+++ Assets/CardHand.cs
@@ -7,6 +7,9 @@ public class CardHand : MonoBehaviour
 
     public Cards[] hand;
 
+    public int my_actionpoints;
+    public bool is_my_turn;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Cards.cs
+++ Assets/Cards.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Cards : MonoBehaviour
 {
+
+    public int ap_cost;
+    public int card_id;
+
+
+    private void OnMouseDown()
+    {
+        if (card_id == 1)
+        {
+
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
