bddc2d91fb7d169ae155d96f144b7abde75db509
E-LAMB <aatkinson2022@elam.co.uk>
2023-12-06 10:47:53 +0000

    Cards

--- Assets/Cards.cs
+++ Assets/Cards.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MakeupPath : MonoBehaviour
+public class Cards : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
